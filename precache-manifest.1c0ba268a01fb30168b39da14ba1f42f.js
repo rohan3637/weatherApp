e44d3ee915a29b767da2f6e6ab3a7d1b213c52d6
Rohan Kumar <yadavrohan829@gmail.com>
2021-03-13 16:00:16 +0530

    Updates

---
+++ precache-manifest.1c0ba268a01fb30168b39da14ba1f42f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f11bd2b346d4f0286278d2d1f3a22ae",
+    "url": "/rohan3637/weatherAppindex.html"
+  },
+  {
+    "revision": "57038fc55a1f16479614",
+    "url": "/rohan3637/weatherAppstatic/css/main.7b8b0fce.chunk.css"
+  },
+  {
+    "revision": "6609914bd8224de241a9",
+    "url": "/rohan3637/weatherAppstatic/js/2.6b90bb15.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/rohan3637/weatherAppstatic/js/2.6b90bb15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57038fc55a1f16479614",
+    "url": "/rohan3637/weatherAppstatic/js/main.df400e14.chunk.js"
+  },
+  {
+    "revision": "a4a7cb62a1bfe8c2388a",
+    "url": "/rohan3637/weatherAppstatic/js/runtime-main.38b4628c.js"
+  },
+  {
+    "revision": "044f71bb11bced17c006eb680ae3e6bb",
+    "url": "/rohan3637/weatherAppstatic/media/hot.044f71bb.png"
+  },
+  {
+    "revision": "88e10ef03528e26f22f110303e5f3ac0",
+    "url": "/rohan3637/weatherAppstatic/media/map-pointer.88e10ef0.png"
+  },
+  {
+    "revision": "4ca76975d80b861ba16c3a87bb7c1143",
+    "url": "/rohan3637/weatherAppstatic/media/sun.4ca76975.png"
+  }
+]);
